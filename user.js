c2c52e304dd0b39baa73d188879f55a159f00300
fairy2853 <nazik220@ukr.net>
2022-02-18 18:55:41 +0200

    asdasd

---
+++ user.js
@@ -0,0 +1,43 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+let profile = document.getElementById('profile');
+
+const url = "http://my-json-server.typicode.com/fairy2853/NAS"
+
+let userRequest =new XMLHttpRequest();
+
+
+
+userRequest.open("GET",`${url}/users/${id}`);
+userRequest.responseType="json";
+userRequest.onload=function(){
+    let user=userRequest.response;
+  
+    profile.innerHTML = `
+
+        <h1>${user.name}</h1>
+
+        <h2>${user.sirname}</h2>
+
+        <img class="profile-img" src="${user.photo_url}">
+
+        <p>Balance: ${user.balance}$</p>
+
+    `
+
+
+};
+
+    
+
+    
+    
+
+
+
+
+
+userRequest.send();
+
