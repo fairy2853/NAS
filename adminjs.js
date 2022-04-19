be035e2e1a9fcbff904a93776a411f8a4f425969
fairy2853 <nazik220@ukr.net>
2022-04-19 17:58:31 +0300

    asd

--- adminjs.js
+++ adminjs.js
@@ -1,28 +1,27 @@
-let productForm = document.getElementById('container');
+let productForm = document.getElementById('add_product_form');
 
-productForm.addEventListener('submit',function(event){
-   event.preventDefault();
-   let data = JSON.stringify({
-      "name":event.target['name'].value, 
-      "description":event.target['description'].value,
-      "price":event.target['price'].value,
-      "photo_url":event.target['photo_url'].value   
-   });
+productForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let data = JSON.stringify({
+        "name": event.target['name'].value,
+        "description": event.target['description'].value,
+        "price": event.target['price'].value,
+        "photo_url": event.target['photo_url'].value
+    });
 
-   let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
 
-   xhr.withCredentials= false;
+    // &&&&&
+    xhr.withCredentials = false;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
 
-   xhr.addEventListener("readystatechange",function(){
-    if(this.readyState=== 4 ){
-        console.log(this.responseText);
-    }
-
-   });
-   xhr.open("POST","https://nasdb-393c.restdb.io/rest/products");
-   xhr.setRequestHeader("content-type", "application/json");
-   xhr.setRequestHeader("x-apikey", "62471b7167937c128d7c9406");
-   xhr.setRequestHeader("cache-control", "no-cache");
-   xhr.send(data);
-})
-
+    xhr.open("POST", "https://nasdb-393c.restdb.io/rest/product");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "62471b7167937c128d7c9406");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.send(data);
+});
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let xhr = new XMLHttpRequest();
 let url = "https://nasdb-393c.restdb.io/rest";
 
 
-xhr.open('GET',url + '/products');
+xhr.open('GET',url + '/product');
 xhr.setRequestHeader("content-type", "application/json");
 xhr.setRequestHeader("x-apikey", "62471b7167937c128d7c9406");
 xhr.setRequestHeader("cache-control", "no-cache");
@@ -31,13 +31,6 @@ xhr.onload = function() {
 }
 xhr.send();
 
-function addProductToCart(id) {
-    xhr.open('GET',`${url}/products/${id}`);
-    xhr.responseType = 'json'
-    xhr.onload = function() {
-
-    }
-}
 
 // CART ----------------
 
@@ -52,7 +45,7 @@ if(localStorage.getItem('cart')) {
 
 function addProductToCart(id) {
     let product = productsArray.find(function(p) {
-        return p.id == id;
+        return p._id == id;
     })
     cart.push(product);
     drawCartProducts();
@@ -69,6 +62,7 @@ function drawCartProducts() {
     cartProd.innerHTML = null;
     let sum = 0;
     cart.forEach(function(p){
+        
         cartProd.innerHTML += `
             <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
             <hr>
@@ -81,12 +75,81 @@ function drawCartProducts() {
     `;
 }
 
+let orderBlock = document.getElementById('order-block');
+// Get the modal
+let modal = document.getElementById('myModal');
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+
+  }
+}
+
+    let contentGrid = document.getElementById('content-grid');
+    let form = document.getElementById('form');
+
+function addProductToModal(){
+    let totalPrice=0;
+    cart.forEach(p =>{
+        let modaldiv = document.createElement('div'); 
+        modaldiv.classList.add('contentProduct');
+       
+        modaldiv.innerHTML += `
+        <img src="${p.photo_url}">
+        <p> ${p.name} |${p.price}$</p> `
+        totalPrice+=p.price;
+        contentGrid.append(modaldiv);
+        
+    });
+    form.innerHTML=null;
+     form.innerHTML+=`
+         <form id="buy_product_form">
+                <div class="form-group">
+                    <input class="buy-product-form" name="Full name" type="text" placeholder="Full Name">
+                </div>
+
+                <div class="form-group">
+                    <input class="buy-product-form" name="Delivery Adress"  type="text" placeholder="Delivery Adress">
+                </div>
+
+                 <div class="form-group">
+                    <input class="buy-product-form" name="Phone Number"  type="text" placeholder="Phone Number">
+                </div>
+
+                <div class="form-group">
+                     <input class="buy-product-form" name="Post Office Number" type="text" placeholder="Post Office number" >
+                </div>
+                    <p>${totalPrice}$</p>
+                    <br>
+                <button id="order" >Order</button>
+   
+        </form>     
+     `
+      
+}
+
+
+
+
 function buyAll() {
-    cart = [];
-    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    modal.style.display="block";
+    addProductToModal();
+    cart=[];    
     localStorage.setItem("cart", '[]');
+
+  
 }
 
 function openCart() {
     cartProd.classList.toggle('hide');
 }
+
